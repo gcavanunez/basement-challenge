0c3775ff4d79872d6bfb57e725dc37745362cf54
Guillermo Antony Cava Nuñez <gcavanunez@gmail.com>
2021-08-23 13:24:43 -0500

    open graphql added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,34 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <GlobalProvider>
         <Head>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
           <title>Basement studio - Challenge</title>
+          <meta name="theme-color" content="#000000"></meta>
+          <meta
+            name="description"
+            content="A man can’t have enough base­ment swag"
+          />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Basement studio - Challenge" />
+          <meta
+            name="twitter:description"
+            content="A man can’t have enough base­ment swag"
+          />
+          <meta name="twitter:site" content="@gcavanunez" />
+          <meta property="twitter:image" content="/open-graphql.jpg"></meta>
+          <meta name="og:title" content="Basement studio - Challenge" />
+          <meta
+            name="og:description"
+            content="A man can’t have enough base­ment swag"
+          />
+          <meta
+            name="og:url"
+            content="https://basement-challenge-one.vercel.app"
+          />
+          <meta name="og:site_name" content="Basement studio - Challenge" />
+          <meta name="og:type" content="website" />
+          <meta property="og:image" content="/open-graphql.jpg"></meta>
         </Head>
         <div className="min-h-screen">
           <Header />
